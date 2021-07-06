2787dc2dd23fe773d0d009986e361f1407683127
Rubén Conde <rubencondemag96@gmail.com>
2021-07-07 01:59:02 +0200

    feat: Compression

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { FastifyAdapter, NestFastifyApplication } from '@nestjs/platform-fastify';
+import compression from 'fastify-compress';
 import { AppModule } from './app.module';
 import { validationPipe } from './config/pipes/validation.pipes';
 
 async function bootstrap() {
    const app = await NestFactory.create<NestFastifyApplication>(AppModule, new FastifyAdapter());
    app.useGlobalPipes(validationPipe);
+   app.use(compression);
    await app.listen(3000, '0.0.0.0');
 }
 bootstrap();

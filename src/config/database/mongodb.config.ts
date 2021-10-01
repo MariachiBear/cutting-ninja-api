b7407faf219691b9a13dfbc33ef35d2a893f8b9e
Rubén Conde <ruben.conde@ux-key.com>
2021-10-01 09:54:41 +0200

    fix: :bug: MongoDB configuration fix due to dependency upgrade

--- src/config/database/mongodb.config.ts
+++ src/config/database/mongodb.config.ts
@@ -3,8 +3,6 @@ import { MongooseModuleOptions } from '@nestjs/mongoose';
 export const mongodbConfig: MongooseModuleOptions = {
    useFactory: () => ({
       uri: process.env.MONGODB_URI,
-      useCreateIndex: true,
-      useFindAndModify: false,
       useNewUrlParser: true,
       useUnifiedTopology: true,
    }),

e3c9bab100bb4ceecf9e3f381cb9835e3f3c67a6
Rubén Conde <ruben.conde@ux-key.com>
2021-10-11 16:52:25 +0200

    chore: :wrench: Swagger tags added to general config

--- src/config/swagger/swagger.config.ts
+++ src/config/swagger/swagger.config.ts
@@ -4,6 +4,10 @@ export const swaggerConfig = new DocumentBuilder()
    .setTitle('URL Shortener')
    .setDescription('API REST for the url shortener')
    .setVersion('1.0')
+   .addTag('Root')
+   .addTag("URL's")
+   .addTag('Users')
+   .addTag('Visits')
    .build();
 
 export const swaggerDocConfig: SwaggerDocumentOptions = {};

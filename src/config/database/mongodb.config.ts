bc58845294ab2f813ae63727da329feed6361206
Rubén Conde <ruben.conde@ux-key.com>
2021-10-12 09:06:11 +0200

    refactor(db): :recycle: Changed way to connect to the database

--- src/config/database/mongodb.config.ts
+++ src/config/database/mongodb.config.ts
@@ -2,6 +2,7 @@ import { MongooseModuleOptions } from '@nestjs/mongoose';
 
 export const mongodbConfig: MongooseModuleOptions = {
    useFactory: () => ({
+      dbName: process.env.MONGODB_DB,
       uri: process.env.MONGODB_URI,
       useNewUrlParser: true,
       useUnifiedTopology: true,
